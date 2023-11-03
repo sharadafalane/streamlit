a8a467a9d2a5a03c4595d946c2fcdef928b6297b
sharadafalane <103818085+sharadafalane@users.noreply.github.com>
2023-11-03 22:18:33 +0530

    Update image_transformation.py

--- image_transformation.py
+++ image_transformation.py
@@ -1,30 +1,27 @@
 import streamlit as st
-import cv2
-import numpy as np
+from PIL import Image, ImageOps, ImageFilter
 
-# Function to convert image to grayscale using OpenCV
+# Function to convert image to grayscale using Pillow
 def grayscale_conversion(image):
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    grayscale_image = ImageOps.grayscale(image)
     return grayscale_image
 
-# Function to rotate image using OpenCV
+# Function to rotate image using Pillow
 def rotate_image(image, angle):
-    rows, cols, _ = image.shape
-    M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
-    rotated_image = cv2.warpAffine(image, M, (cols, rows))
+    rotated_image = image.rotate(angle)
     return rotated_image
 
-# Function to flip image using OpenCV
+# Function to flip image using Pillow
 def flip_image(image, direction):
     if direction == 'Horizontal':
-        flipped_image = cv2.flip(image, 1)
+        flipped_image = ImageOps.mirror(image)
     elif direction == 'Vertical':
-        flipped_image = cv2.flip(image, 0)
+        flipped_image = ImageOps.flip(image)
     return flipped_image
 
-# Function to apply Gaussian blur using OpenCV
+# Function to apply Gaussian blur using Pillow
 def gaussian_blur(image, radius):
-    blurred_image = cv2.GaussianBlur(image, (radius*2+1, radius*2+1), 0)
+    blurred_image = image.filter(ImageFilter.GaussianBlur(radius=radius))
     return blurred_image
 
 # Streamlit UI
@@ -35,11 +32,11 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 
 if uploaded_file is not None:
     # Read image
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+    image = Image.open(uploaded_file)
     
     # Display original image
     st.subheader("Original Image")
-    st.image(image, channels="BGR", caption='Original Image', use_column_width=True)
+    st.image(image, caption='Original Image', use_column_width=True)
     
     # Transformation options
     transformation = st.selectbox("Select Transformation", ["Grayscale", "Rotate", "Flip", "Gaussian Blur"])
@@ -48,25 +45,25 @@ if uploaded_file is not None:
         # Apply grayscale conversion
         transformed_image = grayscale_conversion(image)
         st.subheader("Grayscale Image")
-        st.image(transformed_image, channels="BGR", caption='Grayscale Image', use_column_width=True)
+        st.image(transformed_image, caption='Grayscale Image', use_column_width=True)
     
     elif transformation == "Rotate":
         angle = st.slider("Enter Angle (degrees)", -180, 180, 0)
         # Apply rotation
         transformed_image = rotate_image(image, angle)
         st.subheader("Rotated Image")
-        st.image(transformed_image, channels="BGR", caption='Rotated Image', use_column_width=True)
+        st.image(transformed_image, caption='Rotated Image', use_column_width=True)
     
     elif transformation == "Flip":
         direction = st.radio("Select Direction", ["Horizontal", "Vertical"])
         # Apply flipping
         transformed_image = flip_image(image, direction)
         st.subheader("Flipped Image")
-        st.image(transformed_image, channels="BGR", caption='Flipped Image', use_column_width=True)
+        st.image(transformed_image, caption='Flipped Image', use_column_width=True)
     
     elif transformation == "Gaussian Blur":
         radius = st.slider("Enter Blur Radius", 0, 10, 2)
         # Apply Gaussian blur
         transformed_image = gaussian_blur(image, radius)
         st.subheader("Blurred Image")
-        st.image(transformed_image, channels="BGR", caption='Blurred Image', use_column_width=True)
+        st.image(transformed_image, caption='Blurred Image', use_column_width=True)
