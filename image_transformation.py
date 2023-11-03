d7949ab0c9d5f6e7a577c2f0b802190a006569ad
sharadafalane <103818085+sharadafalane@users.noreply.github.com>
2023-11-03 21:49:51 +0530

    Update image_transformation.py

--- image_transformation.py
+++ image_transformation.py
@@ -1,17 +1,10 @@
 import streamlit as st
-import cv2
-import numpy as np
+from PIL import Image, ImageEnhance
 
 def contrast_stretching(image):
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Apply contrast stretching
-    min_val = np.min(gray)
-    max_val = np.max(gray)
-    stretched = ((gray - min_val) / (max_val - min_val) * 255).astype(np.uint8)
-    
-    return stretched
+    enhancer = ImageEnhance.Contrast(image)
+    enhanced_image = enhancer.enhance(2.0)  # Adjust the enhancement factor as needed
+    return enhanced_image
 
 # Streamlit UI
 st.title("Image Enhancement using Contrast Stretching")
@@ -21,15 +14,15 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 
 if uploaded_file is not None:
     # Read image
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+    image = Image.open(uploaded_file)
     
     # Display original image
     st.subheader("Original Image")
-    st.image(image, channels="BGR")
+    st.image(image, caption='Original Image', use_column_width=True)
     
     # Apply contrast stretching
     enhanced_image = contrast_stretching(image)
     
     # Display enhanced image
     st.subheader("Enhanced Image")
-    st.image(enhanced_image, channels="BGR")
+    st.image(enhanced_image, caption='Enhanced Image', use_column_width=True)
